aebd8f531a86b378d3eebdb838e83665f833ebba
ximet <dq.ximet@gmail.com>
2018-02-20 20:41:26 +0300

    full test for method encode\decode

--- tests/index.js
+++ tests/index.js
@@ -27,3 +27,58 @@ describe('test encode method', () => {
         expect(encodedURI).equal('testString%28%29');
     });
 });
+
+describe('encodeQuery', () => {
+    it('encode query with escapeQuerySpace', () => {
+        const encodedQuery = TinyURI.encodeQuery(' ', true);
+
+        expect(encodedQuery).equal('+');
+    });
+
+    it('encode query without escapeQuerySpace', () => {
+        const encodedQuery = TinyURI.encodeQuery(' ', false);
+
+        expect(encodedQuery).equal('%20');
+    });
+});
+
+describe('test decode method', () => {
+    it('simple encode usage', () => {
+        const encodedURI = TinyURI.decode('testString');
+
+        expect(encodedURI).equal('testString');
+    });
+
+    it('decode string with slash', () => {
+        const encodedURI = TinyURI.decode('testString%2F');
+
+        expect(encodedURI).equal('testString/');
+    });
+
+    it('decode string with space', () => {
+        const encodedURI = TinyURI.decode('testString%20');
+
+        expect(encodedURI).equal('testString ');
+    });
+
+    it('decode string with ()', () => {
+        const encodedURI = TinyURI.decode('testString%28%29');
+
+        expect(encodedURI).equal('testString()');
+    });
+});
+
+describe('decodeQuery', () => {
+    it('decode query with escapeQuerySpace', () => {
+        const encodedQuery = TinyURI.decodeQuery('+', true);
+
+        expect(encodedQuery).equal(' ');
+    });
+
+    it('decode query without escapeQuerySpace', () => {
+        const encodedQuery = TinyURI.decodeQuery('%20', false);
+
+        expect(encodedQuery).equal(' ');
+    });
+});
+
